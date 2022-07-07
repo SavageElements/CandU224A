3386aba6d415096df5ab487d987f74cea08d356f
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-07 16:37:49 -0700

    Add project files.

---
+++ CandU224A/Class224A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU224A
+{
+    class Class224A
+    {
+        public static void MathOne(int X)
+        {
+
+            int Answer = (X / 2);
+            Console.WriteLine("Your input divided by two as an int");
+            Console.WriteLine(Answer);
+        }
+
+        public decimal  Y;
+
+        public int DUCK;
+        public static void MathTwo (out string DUCK)
+        {
+            DUCK = "The output string is Duck";
+            Console.WriteLine(DUCK);
+
+        }
+
+        public static void MathOne(int X, int Y, int Z)
+        {
+
+            int Answer = ((X / 2) + Y + Z);
+            Console.WriteLine("Your original input divided by two, plus the nuext two numbers you entered equals");
+            Console.WriteLine(Answer);
+        }
+
+    }
+}
---
+++ CandU224A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CandU224A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int X = Convert.ToInt16(Console.ReadLine());
+
+            Class224A.MathOne(X);
+
+            Class224A.MathTwo(out string Duck);
+
+            Console.WriteLine("Please enter a number");
+            int Y = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please enter a number");
+            int Z = Convert.ToInt16(Console.ReadLine());
+
+            Class224A.MathOne(X, Y, Z);
+
+        }
+    }
+}
